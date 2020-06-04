be09b1f62a726b61263f2e40002461c6dd7a3f82
Deryck Jackson <44139628+DeryckJackson@users.noreply.github.com>
2020-06-04 13:20:02 -0700

    add html content and skeleton JS code
    Co-authored by: Ben White <bwhite2140@outlook.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,20 @@
 $(document).ready(function() {
-  
+  $("#form").submit(function(event) {
+    event.preventDefault();
+    const input = $("input:radio[name=animal]:checked").val();
+
+    if (input === "goat") {
+      $(".hidden#platypus").hide();
+      $(".hidden#mantisShrimp").hide();
+      $(".hidden#goat").show();
+    } else if (input === "platypus") {
+      $(".hidden#goat").hide();
+      $(".hidden#mantisShrimp").hide();
+      $(".hidden#platypus").show();
+    } else {
+      $(".hidden#goat").hide();
+      $(".hidden#platypus").hide();
+      $(".hidden#mantisShrimp").show();
+    }
+  });
 });
